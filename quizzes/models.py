5edd210990542c51fe8b02fa8b2a65f28ff9c835
Yair <yair.araujo@tecsup.edu.pe>
2025-09-24 15:17:51 -0500

    Creando serializer xd

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
 class Quiz(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(bkank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)  # Título del quiz
+    description = models.TextField(blank=True, null=True)  # Descripción opcional
+    created_at = models.DateTimeField(auto_now_add=True)  # Se guarda la fecha de creación
 
     def __str__(self):
-        return self.title
+        return self.title 
